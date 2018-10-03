119d5785de2ed54f931412218cac2ee00f43d756
Matt Deuschle <mdeuschle@gmail.com>
2018-10-02 20:55:20 -0700

    add Branch1 print

---
+++ Request/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Request
+//
+//  Created by Matt Deuschle on 10/2/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Branch1")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
